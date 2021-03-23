e65d3dcdd701907b5d13dee8f82928376dba76b2
Andrew Felt <75708105+adfelt@users.noreply.github.com>
2021-03-22 21:55:12 -0700

    Added Blog Page

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,15 @@ class Blog(models.Model):
     date_posted = models.DateTimeField()
     text_body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.text_body[:260]
+
+    def pub_date_pretty(self):
+        return self.date_posted.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
+
+
+
